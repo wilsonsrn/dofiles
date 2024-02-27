dc386648ba35cb4c0ac7ca8ca2e83bf8f2902f00
wilsonsrn <75222093+wilsonsrn@users.noreply.github.com>
2024-02-26 22:56:36 -0300

    Update ubuntu-wsl.sh to verbose

--- scripts/ubuntu-wsl.sh
+++ scripts/ubuntu-wsl.sh
@@ -15,41 +15,41 @@ sudo apt upgrade -y
 echo -e "\n\n---> Instalando pacotes. \n"
 sleep 2
 
-# Pacotes importantes.
+echo -e "\n\n---> Pacotes importantes."
 sudo apt install git curl unzip wget fuse libfuse2 zsh cargo -y
 
 cargo --version
 sleep 2
 
-# Torna o ZSH o Shell principal.
+echo -e "\n\n---> Torna o ZSH o Shell principal."
 chsh -s $(which zsh)
 
-# Cria pasta de configuração
+echo -e "\n\n---> Cria pasta de configuração"
 mkdir -p ~/.config
 
-# Instala o Oh-My-ZSH.
+echo -e "\n\n---> Instala o Oh-My-ZSH."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Instala plugins para ZSH.
+echo -e "\n\n---> Instala plugins para ZSH."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Adiciona plugins em ".zshrc".
+echo -e "\n\n---> Adiciona plugins em ".zshrc"."
 sed -i 's/^plugins=\(.*\)/plugins=(\n  git\n  zsh-syntax-highlighting\n  zsh-autosuggestions\n  )/g' ~/.zshrc
 
-# Adiciona alguns aliases em ".zshrc".
+echo -e "\n\n---> Adiciona alguns aliases em ".zshrc"."
 echo '# MY ALIASES' >> ~/.zshrc
 echo 'alias ls="exa --icons --tree --level=2"' >> ~/.zshrc
 echo 'alias vim="lvim"' >> ~/.zshrc
 
-# Instala Starship
+echo -e "\n\n---> Instala Starship"
 curl -sS https://starship.rs/install.sh | sh
 
-# Cria arquivo de configuração do Starship
+echo -e "\n\n---> Cria arquivo de configuração do Starship"
 touch ~/.config/starship.toml
 cp .config/starship.toml ~/.config/
 
-# Pacotes de dependencias Python para desenvolvimento.
+echo -e "\n\n---> Pacotes de dependencias Python para desenvolvimento."
 sudo apt install -y aria2 blt-dev build-essential \
 checkinstall gettext libbz2-dev libc6-dev libffi-dev \
 libgdbm-dev liblzma-dev liblzma-doc libncurses5-dev \
@@ -57,9 +57,9 @@ libncursesw5-dev libnss3-dev libnss3-tools libpq-dev \
 libreadline-dev libsqlite3-dev libssl-dev llvm lzma \
 python3-dev python3-pip python3-venv tcl-dev tk-dev \
 xz-utils zlib1g-dev -y
-# Atualiza PIP
+echo -e "\n\n---> Atualiza PIP"
 pip install --upgrade pip
-# Instala PIPX
+echo -e "\n\n---> Instala PIPX"
 sudo apt install pipx -y
 
 # Instala Rust
@@ -69,40 +69,41 @@ sudo apt install pipx -y
 #source\_cargo\_env\=</span>(source "$HOME/.cargo/env")
 #eval "$source_cargo_env"
 
-# Configuração do Git
+echo -e "\n\n---> Configuração do Git"
 git config --get --global user.email "wilsonsrochaneto@gmail.com"
 git config --get --global user.name "Wilson Rocha Neto"
 git config --global core.editor nvim
 git config --global credential.helper 'cache --timeout=7200'
 
-# Instala Git Credential Manager
+echo -e "\n\n---> Instala Git Credential Manager"
 curl -L https://aka.ms/gcm/linux-install-source.sh | sh
 git-credential-manager configure
 
-# Instala pacote GitHub
+echo -e "\n\n---> Instala pacote GitHub"
 sudo apt install gh -y
 gh auth login -w
 
-# Instala Neovim
+echo -e "\n\n---> Instala Neovim"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 
-# Instala a versão mais recente do nvm
+echo -e "\n\n---> Instala a versão mais recente do nvm"
 NVM_VERSION=$(curl -s "https://api.github.com/repos/nvm-sh/nvm/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash
 
-# Atualiza shell
+echo -e "\n\n---> Atualiza shell"
+sleep 2
 source ~/.bashrc
 
-# Instala o Node mais recente
+echo -e "\n\n---> Instala o Node mais recente"
 nvm install node
 
-# Pacote adicionais em Rust
+echo -e "\n\n---> Pacote adicionais em Rust"
 cargo install ripgrep
 cargo install exa
 
-# Instala LazyGit
+echo -e "\n\n---> Instala LazyGit"
 mkdir -p ~/.stuff
 cd ~/.stuff
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
@@ -111,29 +112,29 @@ tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 cd $dir_installation
 
-# Instala Pyenv
+echo -e "\n\n---> Instala Pyenv"
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 
-# Instala Poetry
+echo -e "\n\n---> Instala Poetry"
 pipx install poetry
 
-# Pacotes bestas.
+echo -e "\n\n---> Pacotes bestas."
 sudo apt install neofetch htop -y
 
-# Instala Tmux e Oh-My-Tmux!
+echo -e "\n\n---> Instala Tmux e Oh-My-Tmux!"
 sudo apt install tmux -y
 git clone https://github.com/gpakosz/.tmux.git "/path/to/oh-my-tmux"
 mkdir -p "~/.config/tmux"
 ln -s "/path/to/oh-my-tmux/.tmux.conf" "~/.config/tmux/tmux.conf"
 cp "/path/to/oh-my-tmux/.tmux.conf.local" "~/.config/tmux/tmux.conf.local"
 
-# Instala LunarVim
+echo -e "\n\n---> Instala LunarVim"
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
-# Baixa Config geral LunarVim
+echo -e "\n\n---> Baixa Config geral LunarVim do repositorio wilsonsrn"
 cd ~/.config
 mv lvim lvim-orig
 git clone https://github.com/wilsonsrn/my-lunarvim.git lvim
