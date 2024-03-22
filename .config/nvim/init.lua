b77a14f3b620c61b1dff1d287186dc05f6df7f3f
wilsonsrn <75222093+wilsonsrn@users.noreply.github.com>
2024-03-21 21:44:25 -0300

    Add wsl clipboard config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Windows/WSL clipboard configuration
+local in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
+
+if in_wsl then
+  vim.g.clipboard = {
+    name = "wsl clipboard",
+    copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
+    paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
+    cache_enabled = true,
+  }
+end
